468a428b300da8b6703558a8c4793865b35c2016
nbulischeck <nbulischeck@users.noreply.github.com>
2018-05-11 12:01:44 -0400

    Removed make artifacts

--- backdoor/.backdoor.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/chirality/minerva-testing/backdoor/src/backdoor/backdoor.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/chirality/minerva-testing/backdoor/src/backdoor/backdoor.ko /home/chirality/minerva-testing/backdoor/src/backdoor/backdoor.o /home/chirality/minerva-testing/backdoor/src/backdoor/backdoor.mod.o ;  true
--- backdoor/.tmp_versions/backdoor.mod
+++
@@ -1,3 +0,0 @@
-/home/chirality/minerva-testing/backdoor/src/backdoor/backdoor.ko
-/home/chirality/minerva-testing/backdoor/src/backdoor/init.o /home/chirality/minerva-testing/backdoor/src/backdoor/nfhook.o /home/chirality/minerva-testing/backdoor/src/backdoor/debugfs.o /home/chirality/minerva-testing/backdoor/src/backdoor/state.o
-
